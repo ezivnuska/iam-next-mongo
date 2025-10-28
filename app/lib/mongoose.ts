2986d149153743220de2fb42fe1cc1b919e18955
Eric Zivnuska <ezivnuska@gmail.com>
2025-10-27 19:47:59 -0700

    added global connection caching.

--- app/lib/mongoose.ts
+++ app/lib/mongoose.ts
@@ -4,7 +4,45 @@ import mongoose from 'mongoose';
 import '@/app/lib/models/image';
 import '@/app/lib/models/user';
 
+if (!process.env.MONGO_URI) {
+  throw new Error('Invalid/Missing environment variable: "MONGO_URI"');
+}
+
+const uri: string = process.env.MONGO_URI;
+
+interface MongooseCache {
+  conn: typeof mongoose | null;
+  promise: Promise<typeof mongoose> | null;
+}
+
+// Extend Node's global type
+declare global {
+  // eslint-disable-next-line no-var
+  var mongoose: MongooseCache | undefined;
+}
+
+// Always ensure cached is initialized
+const cached: MongooseCache = global.mongoose ?? { conn: null, promise: null };
+global.mongoose = cached;
+
 export async function connectToDatabase() {
-  if (mongoose.connections[0].readyState) return;
-  await mongoose.connect(process.env.MONGO_URI!);
+  // Return existing connection
+  if (cached.conn) return cached.conn;
+
+  // Reuse pending connection promise
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+    };
+    cached.promise = mongoose.connect(uri, opts).then((mongoose) => mongoose);
+  }
+
+  try {
+    cached.conn = await cached.promise;
+  } catch (e) {
+    cached.promise = null;
+    throw e;
+  }
+
+  return cached.conn;
 }
