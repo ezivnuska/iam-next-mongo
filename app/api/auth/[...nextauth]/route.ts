4b59a6eab67722b6f5b2bceac08b54a18d679ee7
Eric Zivnuska <ezivnuska@gmail.com>
2026-06-19 21:33:24 -0700

    removed ctx from NextAuth v5 beta's handlers

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,10 +1,10 @@
 import { handlers } from "@/app/lib/auth"
 import { NextRequest } from "next/server"
 
-export function GET(req: NextRequest, ctx: unknown) {
-  return handlers.GET(req, ctx as any)
+export function GET(req: NextRequest) {
+  return handlers.GET(req)
 }
 
-export function POST(req: NextRequest, ctx: unknown) {
-  return handlers.POST(req, ctx as any)
+export function POST(req: NextRequest) {
+  return handlers.POST(req)
 }
