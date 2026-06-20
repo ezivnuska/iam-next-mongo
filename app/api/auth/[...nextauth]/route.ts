e9318f01d82027a4f8e8f1e04e675116e7b910a4
Eric Zivnuska <ezivnuska@gmail.com>
2026-06-19 21:28:12 -0700

    fixed known compat issue between NextAuth v5 beta and Next.js 15's stricter route handler checks.

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,10 @@
-// app/api/auth/[...nextauth]/route.ts
+import { handlers } from "@/app/lib/auth"
+import { NextRequest } from "next/server"
 
-import { handlers } from "@/app/lib/auth";
+export function GET(req: NextRequest, ctx: unknown) {
+  return handlers.GET(req, ctx as any)
+}
 
-export const { GET, POST } = handlers;
+export function POST(req: NextRequest, ctx: unknown) {
+  return handlers.POST(req, ctx as any)
+}
